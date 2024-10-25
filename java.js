cead3bbab1b290312c3cfe2bb3faa675934a938d
matheusballardini <159540170+matheusballardini@users.noreply.github.com>
2024-10-25 10:30:34 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,33 @@
+const jogadores=[
+    {
+        "nome": "Neymar Jr.",
+        "link_foto": "https://example.com/neymar.jpg",
+        "clubes": ["Santos", "Barcelona", "Paris Saint-Germain"],
+        "caracteristicas": ["decisivo", "explosivo", "animado"],
+        "posicao_preferida": "atacante"
+    },
+    {
+        "nome": "Alisson Becker",
+        "link_foto": "https://example.com/alisson.jpg",
+        "clubes": ["Internacional", "Roma", "Liverpool"],
+        "caracteristicas": ["calmo", "determinado"],
+        "posicao_preferida": "goleiro"
+    },
+    {
+        "nome": "Virgil van Dijk",
+        "link_foto": "https://example.com/van_dijk.jpg",
+        "clubes": ["Celtic", "Southampton", "Liverpool"],
+        "caracteristicas": ["valentão", "calmo", "decisivo"],
+        "posicao_preferida": "defensor"
+    },
+    {
+        "nome": "Kevin De Bruyne",
+        "link_foto": "https://example.com/de_bruyne.jpg",
+        "clubes": ["Chelsea", "Werder Bremen", "Manchester City"],
+        "caracteristicas": ["determinado", "decisivo", "animado"],
+        "posicao_preferida": "meio campista"
+    }
+]
+
+//acessar uma propriedade
+jogadores[3].link_foto
